f032999d64330ecf634748088abcbe4b4b437cce
wooYeon-gitHub <dndus9510@gmail.com>
2023-04-03 15:03:46 +0900

    고해상도 영화 포스터 출력 및 뒤로가기 검색어 복구

--- dist/index.4d6bcbeb.js
+++ dist/index.4d6bcbeb.js
@@ -773,7 +773,9 @@ class Search extends (0, _kato.Component) {
     render() {
         this.el.classList.add("search");
         this.el.innerHTML = /* html */ `
-      <input placeholder="Enter the movie title to search!"/>
+      <input 
+      value="${(0, _movieDefault.default).state.searchText}" 
+      placeholder="Enter the movie title to search!"/>
       <button class="btn btn-primary">
         Search!
       </button>
@@ -946,10 +948,11 @@ class Movie extends (0, _kato.Component) {
         await (0, _movie.getMovieDetails)(history.state.id);
         console.log((0, _movieDefault.default).state.movie);
         const { movie  } = (0, _movieDefault.default).state;
+        const bigPoster = movie.Poster.replace("SX300", "SX700");
         this.el.classList.add("container", "the-movie");
         this.el.innerHTML = /* html */ `
       <div 
-      style="background-image: url(${movie.Poster})" 
+      style="background-image: url(${bigPoster})" 
       class="poster"></div>
       <div class="specs">
         <div class="title">
