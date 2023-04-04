7930a137270116212d7aa3a61eef183c750a9e82
wooYeon-gitHub <dndus9510@gmail.com>
2023-04-04 13:41:00 +0900

    영화 상세 정보 페이지의 스켈레톤 UI 로딩

--- dist/index.4d6bcbeb.js
+++ dist/index.4d6bcbeb.js
@@ -945,11 +945,19 @@ var _movie = require("../store/movie");
 var _movieDefault = parcelHelpers.interopDefault(_movie);
 class Movie extends (0, _kato.Component) {
     async render() {
+        this.el.classList.add("container", "the-movie");
+        this.el.innerHTML = /* html */ `
+      <div class="poster skeleton"></div>
+      <div class="specs">
+        <div class="title skeleton"></div>
+        <div class="labels skeleton"></div>
+        <div class="plot skeleton"></div>
+      </div>
+    `;
         await (0, _movie.getMovieDetails)(history.state.id);
         console.log((0, _movieDefault.default).state.movie);
         const { movie  } = (0, _movieDefault.default).state;
         const bigPoster = movie.Poster.replace("SX300", "SX700");
-        this.el.classList.add("container", "the-movie");
         this.el.innerHTML = /* html */ `
       <div 
       style="background-image: url(${bigPoster})" 
